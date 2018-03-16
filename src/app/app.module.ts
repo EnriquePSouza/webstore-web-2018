95842d16809244ae7ed5da64821161967a16f216
unknown <enriquepdsz@gmail.com>
2018-03-16 19:23:27 -0300

    Final commit - Version 1.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+// Rotas
+import { Routing, RoutingProviders } from './app.routing';
 
+// Root
 import { AppComponent } from './app.component';
+
+// Shared
 import { HeadbarComponent } from './components/shared/headbar/headbar.component';
 import { SubMenuComponent } from './components/shared/sub-menu/sub-menu.component';
 
+// Components
+import { ProductListComponent } from './components/product-list/product-list.component';
+
+// Pages
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
+
+// Services
+import { CartService } from './services/cart.service';
+import { AuthService } from './services/auth.service';
+
+// Directives
+import { NumberDirective } from './directives/number.directive';
 
 @NgModule({
   declarations: [
+    NumberDirective,
     AppComponent,
     HeadbarComponent,
-    SubMenuComponent
+    SubMenuComponent,
+    ProductListComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    SignupPageComponent,
+    CartPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    Routing
   ],
-  providers: [],
+  providers: [CartService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
